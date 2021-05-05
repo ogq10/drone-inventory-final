3de6b8c94e5515570203eb3aec79185861d86f0e
nwelter1 <nwelter1@gmail.com>
2021-05-05 01:24:05 -0500

    day-3 part1

--- drone_inventory/__init__.py
+++ drone_inventory/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from config import Config
 from .site.routes import site
 from .authentication.routes import auth
-
+from flask_migrate import Migrate
+from .models import db as root_db
 
 app = Flask(__name__)
 
@@ -11,3 +12,7 @@ app.config.from_object(Config)
 app.register_blueprint(site)
 app.register_blueprint(auth)
 
+root_db.init_app(app)
+migrate = Migrate(app, root_db)
+
+from drone_inventory import models
--- drone_inventory/authentication/routes.py
+++ drone_inventory/authentication/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request,flash, redirect, url_for
 from drone_inventory.forms import UserLoginForm
+from drone_inventory.models import User, db
 
 auth = Blueprint('auth', __name__, template_folder='auth_templates')
 
@@ -13,6 +14,19 @@ def signup():
             email = form.email.data
             password = form.password.data
             print(email, password)
+            #insatantiate a new User class here with email/pw
+            user = User(email, password = password)
+            #equivalent to committing or staging this db change
+            db.session.add(user)
+            #equivalent to 'pushing' to our database
+            db.session.commit()
+
+            #Showing a message to the user
+            flash(f'You have successfully created a user account for {email}', 'user-created')
+            
+            return redirect(url_for('site.home'))
+
+
 
     except:
         raise Exception('Invalid form Data: Please check your form')
