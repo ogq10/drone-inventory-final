f7bd5cf95ae3d5e4526497f252929bf41e0bb3aa
nwelter1 <nwelter1@gmail.com>
2021-05-04 21:22:34 -0500

    day-2

--- drone_inventory/authentication/routes.py
+++ drone_inventory/authentication/routes.py
@@ -1,11 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from drone_inventory.forms import UserLoginForm
 
 auth = Blueprint('auth', __name__, template_folder='auth_templates')
 
 
 @auth.route('/signup', methods = ['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    form = UserLoginForm()
+    
+    try:
+        if request.method == 'POST' and form.validate_on_submit():
+            email = form.email.data
+            password = form.password.data
+            print(email, password)
+
+    except:
+        raise Exception('Invalid form Data: Please check your form')
+
+    return render_template('signup.html', form = form)
+
 
 @auth.route('/signin', methods = ['GET', 'POST'])
 def signin():
